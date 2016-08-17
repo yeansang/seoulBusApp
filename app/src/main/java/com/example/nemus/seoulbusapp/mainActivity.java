855de5d4b825361cd31ba4c6fa6b7774d301db63
yeansang ahn <ysan1991@gmail.com>
2016-08-17 18:50:48 +0900

    location get test

--- app/src/main/java/com/example/nemus/seoulbusapp/mainActivity.java
+++ app/src/main/java/com/example/nemus/seoulbusapp/mainActivity.java
@@ -1,8 +1,11 @@
 package com.example.nemus.seoulbusapp;
 
+import android.*;
+import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.support.v4.app.ActivityCompat;
@@ -21,6 +24,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class mainActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationManager mLocationManager;
 
     private double startPoint[] = new double[]{37.558345, 126.994583};
 
@@ -29,13 +33,19 @@ public class mainActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         int permissionCheck = 0;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            String[] requstPermission = new String[]{""};
+            String[] requstPermission = new String[]{"",""};
             permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION);
             if (permissionCheck == PackageManager.PERMISSION_DENIED) {
                 requstPermission[0] = android.Manifest.permission.ACCESS_FINE_LOCATION;
             }
+            permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION);
+            if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+                requstPermission[1] = Manifest.permission.ACCESS_COARSE_LOCATION;
+            }
             requestPermissions(requstPermission, 0);
         }
+        mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,10000,0,new MyLocationListener());
         setContentView(R.layout.activity_main);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -57,39 +67,33 @@ public class mainActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-        if (lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            Location ll = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            if (ll != null) {
-                startPoint[0] = ll.getLatitude();
-                startPoint[1] = ll.getLongitude();
-                Log.d("point", startPoint[0] + "/" + startPoint[1]);
-            }
-        }
-        String locationProvider = LocationManager.GPS_PROVIDER;
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-
-            return;
-        }
-        Location lastKnownLocation = lm.getLastKnownLocation(locationProvider);
-        if (lastKnownLocation != null) {
-            double lng = lastKnownLocation.getLatitude();
-            double lat = lastKnownLocation.getLatitude();
-            Log.d("Main", "longtitude=" + lng + ", latitude=" + lat);
-        }
-
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(startPoint[0], startPoint[1]);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,15.5f));
+    }
+
+    class MyLocationListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            startPoint[0] = location.getLatitude();
+            startPoint[1] = location.getLongitude();
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
     }
 }
